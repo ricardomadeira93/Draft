255d65d621cc430968dca57e92bf10a26ebcc5b8
Ricardo Madeira <madeiragenterprise@gmail.com>
2026-04-26 15:53:58 -0300

    refactor: update i18n configuration to use requestLocale with a default fallback to English

--- draft-web/i18n.ts
+++ draft-web/i18n.ts
@@ -4,9 +4,12 @@ import {notFound} from 'next/navigation';
 export const locales = ['en', 'pt'] as const;
 export type Locale = (typeof locales)[number];
 
-export default getRequestConfig(async ({locale}) => {
-  // Validate that the incoming `locale` parameter is valid
-  if (!locales.includes(locale as Locale)) notFound();
+export default getRequestConfig(async ({requestLocale}) => {
+  let locale = await requestLocale;
+
+  if (!locale || !locales.includes(locale as Locale)) {
+    locale = 'en';
+  }
 
   let messages;
   if (locale === 'pt') {
