4f7c0ef67764f7a424fc7c1a6a154b05530fb379
Ricardo Madeira <madeiragenterprise@gmail.com>
2026-04-26 12:00:52 -0300

    Fix: convert rfpilot-web from submodule to directory

--- rfpilot-web/components/theme-provider.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import * as React from "react";
-import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { type ThemeProviderProps } from "next-themes"; // Corrected import path for Next.js 15 / next-themes v0.4
-
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
-}
